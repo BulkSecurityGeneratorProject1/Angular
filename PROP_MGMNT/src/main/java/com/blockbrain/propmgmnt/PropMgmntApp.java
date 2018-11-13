a98c32d17de8153930de3bdcc4560ec02913f249
kartikeya <kartikeya_g@rediffmail.com>
2018-11-13 09:12:44 +0530

    postgres support and mqtt integration

--- PROP_MGMNT/src/main/java/com/blockbrain/propmgmnt/PropMgmntApp.java
+++ PROP_MGMNT/src/main/java/com/blockbrain/propmgmnt/PropMgmntApp.java
@@ -2,34 +2,58 @@ package com.blockbrain.propmgmnt;
 
 import com.blockbrain.propmgmnt.config.ApplicationProperties;
 import com.blockbrain.propmgmnt.config.DefaultProfileUtil;
+import com.blockbrain.propmgmnt.domain.Apartment;
+import com.blockbrain.propmgmnt.domain.enumeration.ApartmentStatus;
+import com.blockbrain.propmgmnt.repository.ApartmentRepository;
 
 import io.github.jhipster.config.JHipsterConstants;
 
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Pollers;
+import org.springframework.integration.endpoint.MessageProducerSupport;
+import org.springframework.integration.handler.LoggingHandler;
+import org.springframework.integration.handler.ServiceActivatingHandler;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.integration.stream.CharacterStreamReadingMessageSource;
+import org.springframework.messaging.MessageHandler;
 
 import javax.annotation.PostConstruct;
 import java.net.InetAddress;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Optional;
 
 @SpringBootApplication
 @EnableConfigurationProperties({LiquibaseProperties.class, ApplicationProperties.class})
 public class PropMgmntApp {
 
     private static final Logger log = LoggerFactory.getLogger(PropMgmntApp.class);
+    
+    @Autowired
+    private ApartmentRepository apartmentRepository;
 
     private final Environment env;
+
 
     public PropMgmntApp(Environment env) {
         this.env = env;
     }
-
+    
     /**
      * Initializes PROP_MGMNT.
      * <p>
@@ -82,4 +106,81 @@ public class PropMgmntApp {
             env.getProperty("server.port"),
             env.getActiveProfiles());
     }
+    
+    @Bean
+	public MqttPahoClientFactory mqttClientFactory() {
+		DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+		MqttConnectOptions options = new MqttConnectOptions();
+		//options.setServerURIs(new String[] { "tcp://localhost:1883" });
+		options.setServerURIs(new String[] { "tcp://m15.cloudmqtt.com:17499"});
+		//options.setUserName("guest");
+		options.setUserName("crjhwzok");
+		//options.setPassword("guest".toCharArray());
+		options.setPassword("G7aPp3DZ-2bi".toCharArray());
+		factory.setConnectionOptions(options);
+		return factory;
+	}
+
+	// publisher
+
+	@Bean
+	public IntegrationFlow mqttOutFlow() {
+		return IntegrationFlows.from(CharacterStreamReadingMessageSource.stdin(),
+						e -> e.poller(Pollers.fixedDelay(1000)))
+				.transform(p -> p + " sent to MQTT")
+				.handle(mqttOutbound())
+				.get();
+	}
+
+	@Bean
+	public MessageHandler mqttOutbound() {
+		MqttPahoMessageHandler messageHandler = new MqttPahoMessageHandler("siSamplePublisher", mqttClientFactory());
+		messageHandler.setAsync(true);
+		//messageHandler.setDefaultTopic("siSampleTopic");
+		messageHandler.setDefaultTopic("Sensor");
+		return messageHandler;
+	}
+
+	// consumer
+
+	@Bean
+	public IntegrationFlow mqttInFlow() {
+		return IntegrationFlows.from(mqttInbound())
+				.transform(p -> p + ", received from MQTT")
+				.handle(requestPersistingHandler())
+				.handle(logger())
+				.get();
+	}
+	
+	@Bean
+	public MessageHandler requestPersistingHandler() {
+	    return new ServiceActivatingHandler(message -> {
+	        Optional<Apartment> apartment = apartmentRepository.findById(new Long(1));
+	        if(apartment.isPresent()) {
+	        		Apartment apt = apartment.get();
+	        		apt.setUnitStatus(ApartmentStatus.Rented);
+	        	apartmentRepository.save(apt);
+	        }
+	        return message.getPayload();	
+	    });
+	}
+
+
+	private LoggingHandler logger() {
+		LoggingHandler loggingHandler = new LoggingHandler("INFO");
+		loggingHandler.setLoggerName("siSample");
+		return loggingHandler;
+	}
+
+	@Bean
+	public MessageProducerSupport mqttInbound() {
+		//MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter("siSampleConsumer",
+		//		mqttClientFactory(), "siSampleTopic");
+		MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter("siSampleConsumer",
+				mqttClientFactory(), "switch");
+		adapter.setCompletionTimeout(5000);
+		adapter.setConverter(new DefaultPahoMessageConverter());
+		adapter.setQos(1);
+		return adapter;
+	}
 }
